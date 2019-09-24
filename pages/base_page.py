861520fb55bb793706b46c313b06b1ad282947d2
Victoria <v.goncearova@gmail.com>
2019-09-24 22:33:45 +0300

    Create base page and main page files

---
+++ pages/base_page.py
@@ -0,0 +1,10 @@
+__author__ = "victoria"
+
+class BasePage():
+
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)

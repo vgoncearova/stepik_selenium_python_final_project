3abda79a0e5c40c4105959ac5f8bcf01aada87a6
Victoria <v.goncearova@gmail.com>
2019-09-25 23:15:12 +0300

    Add test for checking presence of element on the page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,1 +1,14 @@
 __author__ = "victoria"
+
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+class MainPage(BasePage):
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
